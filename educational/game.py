9b6d86a346e1e7488f89269e2813bbee3589008b
Beth Werbaneth <werbae@rpi.edu>
2011-10-31 20:59:37 -0400

    a great effort

--- educational/game.py
+++ educational/game.py
@@ -14,6 +14,12 @@ class game():
 		self.initialize_screen()
 		self.frametime = 0.0
 		self.no_exit
+		self.distance = 0
+		self.lives = 5
+		self.immortal = 2000
+		self.last_death = -2000
+		self.player_killed = False
+		self.in_level = False
 		
 	def initialize_screen(self):
 		"""Initialize the window"""
@@ -29,6 +35,31 @@ class game():
 	
 	def update(self):
 		"""Update every frame"""
+		#update player
+		self.distance += self.frametime
+		projectiles = []
+		if self.lives > 0:
+			projectiles = self.player.update(self.frametime)
+		else
+			txtsurf = self.font32.render("GAME OVER", 1, (0,0,0))
+            txtrect = txtsurf.get_rect()
+            txtrect.center = (300, self.windowy/2)
+            self.screen.blit(txtsurf, txtrect)
+            pygame.display.flip()
+            pygame.time.wait(2000)
+            self.exit_game()
+		if self.player_killed == True:
+			if self.distance > self.last_death + self.immortal_time:
+				self.last_death = self.distance
+				self.lives -= 1
+				self.player_killed = False
+			else:
+				self.player_killed = False
+		#update enemies
+		
+		#handle collision
+		self.handle_collision(projectiles)
+		
 		
 	def handle_collision(self, projectiles):
 		"""handle all collision"""
@@ -37,6 +68,38 @@ class game():
 		
 	def handle_events(self):
 		"""Handle events (key presses, etc.)"""
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				self.exit_game()
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					self.exit_game()
+				if self.in_level == True:
+					#movement
+					if event.key in self.key_bindings.up:
+                        self.player.moving[0] = True
+                    if event.key in self.key_bindings.down:
+                        self.player.moving[1] = True
+                    if event.key in self.key_bindings.left:
+                        self.player.moving[2] = True
+                    if event.key in self.key_bindings.right:
+                        self.player.moving[3] = True
+                    #abilities
+                    if event.key in self.key_bindings.shoot:
+                        self.player.shoot = True
+			if event.type == pygame.KEYUP:
+				#cancelling movement
+                if event.key in self.key_bindings.up:
+                    self.player.moving[0] = False
+                if event.key in self.key_bindings.down:
+                    self.player.moving[1] = False
+                if event.key in self.key_bindings.left:
+                    self.player.moving[2] = False
+                if event.key in self.key_bindings.right:
+                    self.player.moving[3] = False
+				#cancelling abilities
+                if event.key in self.key_bindings.shoot:
+                    self.player.shoot = False
 		
 	
 	def run(self, already_run):
---
+++ educational/key_bindings.py
@@ -0,0 +1,21 @@
+import pygame
+
+class key_bindings():
+
+    def __init__(self):
+        self.up = [pygame.K_UP, pygame.K_w]
+        self.down = [pygame.K_DOWN, pygame.K_s]
+        self.left = [pygame.K_LEFT, pygame.K_a]
+        self.right = [pygame.K_RIGHT, pygame.K_d]
+        self.shoot = [pygame.K_SPACE, None]
+
+    @classmethod
+    def get_list(cls, list):
+        return cls.get_string(list[0]) + ", " + cls.get_string(list[1])
+
+    @classmethod
+    def get_string(cls, key):
+        if key == None:
+            return "None"
+        else:
+            return pygame.key.name(key)
